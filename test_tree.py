1311fee6577ae148e2b74663d84167dde6b41b82
slancheros <sandrallancherosg@gmail.com>
2019-09-16 00:47:10 -0500

    Fixed bug, eliminating path leaves

--- test_tree.py
+++ test_tree.py
@@ -3,30 +3,49 @@ from tree import Tree
 
 
 class TestTree(TestCase):
-
     myTree = None
 
     def setUp(self) -> None:
         self.myTree = Tree(5, [1, 2, 3, 2, 3], [(1, 2), (2, 3), (2, 4), (1, 5)])
 
     def test_calculate_amount_colors(self):
-        count_colors = self.myTree.calculate_amount_colors(1,2)
+        count_colors = self.myTree.calculate_amount_colors(1, 2)
+        self.assertEqual(count_colors, 2)
+
+    def test_calculate_amount_colors_1_3(self):
+        count_colors = self.myTree.calculate_amount_colors(1, 3)
+        self.assertEqual(count_colors, 3)
+
+    def test_calculate_amount_colors_with_longer_path(self):
+        count_colors = self.myTree.calculate_amount_colors(1, 4)
+        self.assertEqual(count_colors, 2)
+
+    def test_calculate_amount_colors_with_longer_1_5(self):
+        count_colors = self.myTree.calculate_amount_colors(1, 5)
         self.assertEqual(count_colors, 2)
 
     def test_find_path_bfs(self):
-        path_to_test = self.myTree.find_path_bfs(1,4)
+        path_to_test = self.myTree.find_path_bfs(1, 4)
         self.assertEqual(path_to_test, [(1, 2), (2, 4)])
 
+    def test_find_basic_path_bfs(self):
+        path_to_test = self.myTree.find_path_bfs(1, 1)
+        self.assertEqual(path_to_test, [(1, 1)])
+
+    def test_find_path_bfs_1_3(self):
+        path_to_test = self.myTree.find_path_bfs(1, 3)
+        self.assertEqual(path_to_test, [(1, 2), (2, 3)])
+
     def test_is_leaf(self):
-        is_leaf = self.myTree.is_leaf((2,4),2)
+        is_leaf = self.myTree.is_leaf((2, 4), 2)
         self.assertTrue(is_leaf)
 
     def test_is_not_leaf(self):
-        is_leaf= self.myTree.is_leaf((1,2),1)
+        is_leaf = self.myTree.is_leaf((1, 2), 1)
         self.assertFalse(is_leaf)
 
     def test_contains_edge(self):
-        contains_number = Tree.contains_edge(1,(1,2))
+        contains_number = Tree.contains_edge(1, (1, 2))
         self.assertTrue(contains_number)
 
     def test_does_not_contain_edge(self):
@@ -35,11 +54,19 @@ class TestTree(TestCase):
 
     def test_calculate_sum_for_node_simple(self):
         sum_for_node = self.myTree.calculate_sum_for_node(1)
-        self.assertEqual(sum_for_node, 10)
+        self.assertEqual(10, sum_for_node)
+
+    def test_calculate_sum_for_complex(self):
+        sum_for_node = self.myTree.calculate_sum_for_node(3)
+        self.assertEqual(11, sum_for_node)
 
     def test_calculate_tree_sum(self):
         self.myTree.calculate_tree_sum()
-        self.assertEqual(self.myTree.colors_per_node_list, [10,9,11,9,12])
+        self.assertEqual(self.myTree.colors_per_node_list, [10, 9, 11, 9, 12])
 
-
-
+    def test_remove_leaves_in_path(self):
+        path = [(3,2),(1,2),(2,4)]
+        i=3
+        j=4
+        self.myTree.remove_leaves_in_path(path, i, j)
+        self.assertEqual([(3, 2), (2, 4)], path)
--- tree.py
+++ tree.py
@@ -11,28 +11,10 @@ class Tree:
         color_list = list()
         for tuple_element in path_list:
             for id in tuple_element:
-                color = self.nodes[id]
+                color = self.nodes[(id-1)]
                 if color not in color_list:
                     color_list.append(color)
         return len(color_list)
-
-    # def find_path_bfs(self, i, j):
-    #     path = list()
-    #     if i == j:
-    #         path.append((i,j))
-    #     else:
-    #         filtered_edges = filter(lambda x: self.contains_edge(i, x), self.edges)
-    #         list_filtered_edges = list(filtered_edges)
-    #         for current_edge in list_filtered_edges:
-    #             if self.contains_edge(j, current_edge):
-    #                 path.append(current_edge)
-    #             elif not self.is_leaf(current_edge,i):
-    #                 next_source = filter(lambda x: x != i, current_edge)
-    #                 for item_next_source in next_source:
-    #                     int_next_source = item_next_source
-    #                     path.append(current_edge)
-    #                     path.append(self.find_path_bfs(int_next_source, j))
-    #     return path
 
     def find_path_bfs(self,i,j):
         path = list()
@@ -43,22 +25,42 @@ class Tree:
             filter_edges_with_i = filter(lambda x: self.contains_edge(i, x), self.edges)
             edges_to_visit= list(filter_edges_with_i)
             while len(edges_to_visit) > 0:
-                for current_edge in edges_to_visit:
-                    visited.append(current_edge)
-                    if self.contains_edge(j,current_edge):
-                        path.append(current_edge)
-                        edges_to_visit.clear()
-                    elif not self.is_leaf(current_edge,i):
-                        path.append(current_edge)
-                        next_source = filter(lambda x: x != i, current_edge)
-                        for item_next_source in next_source:
-                            filter_add_to_visit = filter(lambda x: self.contains_edge(item_next_source, x), self.edges)
-                            add_to_visit = list(filter_add_to_visit)
-                            add_to_visit_exclude_visited=[i for i in add_to_visit if i not in visited]
-                            edges_to_visit += add_to_visit_exclude_visited
-                    else:
-                        edges_to_visit.remove(current_edge)
+                if(i,j) in edges_to_visit or (j,i) in edges_to_visit:
+                    path.clear()
+                    if(i, j) in edges_to_visit:
+                        path.append((i,j))
+                    if(j, i) in edges_to_visit:
+                        path.append((j, i))
+                    edges_to_visit.clear()
+                else:
+                    for current_edge in edges_to_visit:
+                        visited.append(current_edge)
+                        if self.contains_edge(j,current_edge):
+                            path.append(current_edge)
+                            edges_to_visit.clear()
+                        elif not self.is_leaf(current_edge,i):
+                            if current_edge not in path:
+                                path.append(current_edge)
+                            next_source = filter(lambda x: x != i, current_edge)
+                            for item_next_source in next_source:
+                                filter_add_to_visit = filter(lambda x: self.contains_edge(item_next_source, x), self.edges)
+                                add_to_visit = list(filter_add_to_visit)
+                                add_to_visit_exclude_visited=[i for i in add_to_visit if i not in visited]
+                                edges_to_visit += add_to_visit_exclude_visited
+                        else:
+                            edges_to_visit.remove(current_edge)
+
+        self.remove_leaves_in_path(path,i,j)
         return path
+
+    def remove_leaves_in_path(self, path, i, j):
+        for tuple_item in path:
+            other_element_filter = filter(lambda x: x != i and x != j, tuple_item)
+            for other_element in other_element_filter:
+                amount_elements_path = filter(lambda x: self.contains_edge(other_element, x), path)
+                list_amount_elements_path = list(amount_elements_path)
+                if len(list_amount_elements_path) == 1:
+                    path.remove(list_amount_elements_path[0])
 
     def is_leaf(self, current_tuple, i):
         other_element_filter = filter(lambda x: x != i, current_tuple)
@@ -79,15 +81,16 @@ class Tree:
 
     def calculate_sum_for_node(self, i):
         sum_colors_node = 0
-        for index in range(1, self.n):
-            sum_colors_node += self.calculate_amount_colors(i, index)
-
-        self.colors_per_node_list.insert(i,sum_colors_node)
+        for index in range(0, self.n):
+            calculated = self.calculate_amount_colors(i, index+1)
+            sum_colors_node += calculated
+        self.colors_per_node_list.insert(index, sum_colors_node)
         return sum_colors_node
 
     def calculate_tree_sum(self):
         for i in range(0, self.n - 1):
             self.calculate_sum_for_node(i+1)
+        print(self.colors_per_node_list)
 
     def display_tree_sum(self):
         for item in self.colors_per_node_list:
